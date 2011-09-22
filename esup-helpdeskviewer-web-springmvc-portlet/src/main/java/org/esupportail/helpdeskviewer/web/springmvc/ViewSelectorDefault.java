24bf57ff4615b31c099702c4d84195280b214773
Vincent Bonamy <vbonamy@gmail.com>
2011-09-22 13:24:46 +0000

    on n'utilise plus wurfl - on lui prefere la detection plus simple a la jasig/uportal
    git-svn-id: https://subversion.cru.fr/helpdeskviewer/trunk@4 971203c4-4fbd-4198-b85e-808f43d49be9

--- esup-helpdeskviewer-web-springmvc-portlet/src/main/java/org/esupportail/helpdeskviewer/web/springmvc/ViewSelectorDefault.java
+++ esup-helpdeskviewer-web-springmvc-portlet/src/main/java/org/esupportail/helpdeskviewer/web/springmvc/ViewSelectorDefault.java
@@ -1,4 +1,26 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.esupportail.helpdeskviewer.web.springmvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.annotation.Resource;
 import javax.portlet.PortletRequest;
@@ -6,9 +28,18 @@ import javax.portlet.WindowState;
 
 import org.springframework.stereotype.Service;
 
-import net.sourceforge.wurfl.core.Device;
-import net.sourceforge.wurfl.core.WURFLManager;
-
+/**
+ * ViewSelectorDefaultImpl provides a default implementation of IViewSelector
+ * that returns JSP view names based on a combination of the browser user agent
+ * string and the portlet window state.  Requests which indicate that the user
+ * is interacting with the portlet via a mobile device, or that the portlet is
+ * currently not in maximized mode will result in a "narrow" view of the 
+ * calendar.  Non-mobile devices using the portlet in the maximized window state
+ * will be shown the "wide" view of the portlet.
+ * 
+ * @author Jen Bourey
+ * @version $Revision: 47637 $
+ */
 @Service
 public class ViewSelectorDefault {
 	
@@ -16,23 +47,36 @@ public class ViewSelectorDefault {
 	private final String HELPDESKVIEWER_NARROW_VIEW = "helpdeskviewerNarrowView";
 	private final String HELPDESKVIEWER_MOBILE_VIEW = "helpdeskviewerMobileView";
 	
+	private List<Pattern> mobileDeviceRegexes = null;
 	
-	@Resource
-	protected WURFLManager wurflManager;
-
+	/**
+	 * Set a list of regex patterns for user agents which should be considered
+	 * to be mobile devices.
+	 * 
+	 * @param patterns
+	 */
+	@Resource(name="mobileDeviceRegexes")
+	public void setMobileDeviceRegexes(List<String> patterns) {
+		this.mobileDeviceRegexes = new ArrayList<Pattern>();
+		for (String pattern : patterns) {
+			this.mobileDeviceRegexes.add(Pattern.compile(pattern));
+		}
+	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.esupportail.portlet.helpdeskviewer.mvc.IViewSelector#getCalendarViewName(javax.portlet.PortletRequest)
 	 */
 	public String getHelpdeskviewerViewName(PortletRequest request) {
-		
 		String userAgent = request.getProperty("user-agent");
 		
-    	Device device = wurflManager.getDeviceForRequest(userAgent);
-    	boolean isWirelessDevice;
-    	if (device.getCapability("is_wireless_device").equals("true")) {
-			return HELPDESKVIEWER_MOBILE_VIEW;
+		// check to see if this is a mobile device
+		if (this.mobileDeviceRegexes != null && userAgent != null) {
+			for (Pattern regex : this.mobileDeviceRegexes) {
+				if (regex.matcher(userAgent).matches()) {
+					return HELPDESKVIEWER_MOBILE_VIEW;
+				}
+			}
 		}
 		
 		// otherwise check the portlet window state
